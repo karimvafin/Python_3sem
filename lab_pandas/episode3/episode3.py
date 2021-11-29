36d283cda68ea3ab71f29d0747c4d0f06cdfd8da
karimvafin <karim.vafin@mail.ru>
2021-11-29 15:42:38 +0300

    Fix issue on pandas::episode3

--- lab_pandas/episode3/episode3.py
+++ lab_pandas/episode3/episode3.py
@@ -7,12 +7,15 @@ df2 = pd.read_excel("students/students_info.xlsx")
 df1.fillna(0, inplace=True)
 df2.dropna(inplace=True)
 
+df = df1.merge(df2, how='inner', left_on='User', right_on='login')
+
 # I part
-df2["Solved"] = [int(df1.loc[df1.User == i].Solved) for i in df2.login]
-faculty_groups = sorted(list(set(df2.group_faculty)))
-groups_out = sorted(list(set(df2.group_out)))
-average_per_faculty_groups = [round(df2.loc[df2.group_faculty == group].Solved.mean(), 1) for group in faculty_groups]
-average_per_groups_out = [round(df2.loc[df2.group_out == group].Solved.mean(), 3) for group in groups_out]
+faculty_groups = df.groupby(['group_faculty']).Solved.mean().index
+average_per_faculty_groups = df.groupby(['group_faculty']).Solved.mean().values
+
+groups_out = df.groupby(['group_out']).Solved.mean().index
+average_per_groups_out = df.groupby(['group_out']).Solved.mean().values
+
 fig, ax = plt.subplots(1, 2)
 
 ax[0].bar(list(map(str, faculty_groups)), average_per_faculty_groups)
@@ -26,6 +29,6 @@ fig.suptitle('Average mark in a group')
 plt.savefig('average_marks.png')
 
 # II part
-users = df1.loc[(df1.H >= 10) | (df1.G >= 10)].User
-print("Номера факультетских групп: ", set([int(df2[df2.login == login].group_faculty) for login in list(users)]))
-print("Номера групп по информатике: ", set([int(df2[df2.login == login].group_out) for login in list(users)]))
+
+print("Номера факультетских групп: ", set(df.loc[(df1.H >= 10) | (df1.G >= 10)].group_faculty))
+print("Номера групп по информатике: ", set(df.loc[(df1.H >= 10) | (df1.G >= 10)].group_out))
