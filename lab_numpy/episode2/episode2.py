f682d45e8da2f17b3bc8f0d0e934613021d11d98
karimvafin <karim.vafin@mail.ru>
2021-11-29 16:27:24 +0300

    Fix issue on numpy::episode2

--- lab_numpy/episode2/episode2.py
+++ lab_numpy/episode2/episode2.py
@@ -6,9 +6,8 @@ fig.set_figheight(8)
 for i in range(1, 4):
     data = np.loadtxt(f"signals/signal0{i}.dat")
     new_data = np.ones(data.shape[0])
-    new_data[0] = data[0]
-    new_data[1:10] = np.array([data[:i].mean() for i in range(1, 10)])
-    new_data[10:] = np.array([data[i - 9:i].mean() for i in range(10, new_data.shape[0])])
+    new_data[0:9] = np.cumsum(data[0:9]) / np.arange(1, 10)
+    new_data[9:] = np.convolve(data, np.ones(10) / 10, mode='valid')
     ax[i-1][0].plot(np.arange(data.shape[0]), data)
     ax[i-1][1].plot(np.arange(data.shape[0]), new_data)
     ax[i-1][0].grid()
